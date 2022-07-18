e3ba34e49087c93c4e5c2633ac437d09e369a2bc
renato <renato.hermoza@pucp.edu.pe>
2022-07-18 05:18:58 -0500

    refresh bindings

--- frontend/bindings/WSMessage.ts
+++ frontend/bindings/WSMessage.ts
@@ -2,6 +2,7 @@
 import type { CupColor } from "./CupColor";
 import type { Question } from "./Question";
 import type { QuestionId } from "./QuestionId";
+import type { QuestionModification } from "./QuestionModification";
 import type { RoomConnectInfo } from "./RoomConnectInfo";
 
-export type WSMessage = { task: "RoomConnect", payload: RoomConnectInfo } | { task: "ChooseCup", payload: CupColor } | { task: "CreateQuestion", payload: Question } | { task: "PublishQuestion", payload: QuestionId } | { task: "DeleteQuestion", payload: QuestionId };
+export type WSMessage = { task: "RoomConnect", payload: RoomConnectInfo } | { task: "ChooseCup", payload: CupColor } | { task: "CreateQuestion", payload: Question } | { task: "PublishQuestion", payload: QuestionId } | { task: "DeleteQuestion", payload: QuestionId } | { task: "ModifyQuestion", payload: QuestionModification };
