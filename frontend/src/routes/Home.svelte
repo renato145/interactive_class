7a1099f397bfa413db066174236919fb0b05113b
renato <renato.hermoza@pucp.edu.pe>
2022-08-08 00:45:02 -0500

    finish delete room ui

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -3,6 +3,7 @@
   import { t } from "svelte-i18n";
   import type { CupsInfo } from "bindings/CupsInfo";
   import type { CreateRoom } from "bindings/CreateRoom";
+  import type { DeleteRoom } from "bindings/DeleteRoom";
 
   const getCups = async () => {
     const d = await fetch("/cups").then((response) => response.json());
@@ -10,7 +11,7 @@
   };
   let cups = getCups();
 
-  let createErrorMsg: string;
+  let createErrorMsg: string, deleteErrorMsg: string;
   const createNewRoom = async (ev) => {
     const new_room = new FormData(ev.target).get("new_room") as string;
     const data: CreateRoom = { new_room };
@@ -28,13 +29,25 @@
     }
   };
 
-  const deleteRoom = async (roomName: string) => {
-    console.log(`deleting ${roomName}`);
+  // index of the room for which to show confirmation dialog for deletion
+  let showConfirmDelete: number = -1;
+
+  const deleteRoom = async (room: string) => {
+    const data: DeleteRoom = { room };
     const response = await fetch("cups/delete_room", {
-      method: 'DELETE',
-      body: roomName
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
     });
-    console.log(response);
+    if (response.ok) {
+      cups = response.json();
+      deleteErrorMsg = undefined;
+      showConfirmDelete = -1;
+    } else {
+      deleteErrorMsg = await response.text();
+    }
   };
 </script>
 
@@ -58,17 +71,35 @@
         {data.rooms.length}
         {data.rooms.length > 1 ? $t("rooms") : $t("room")}
       </p>
+      {#if deleteErrorMsg}
+        <p class="bg-red-200">Error deleting room: "{deleteErrorMsg}"</p>
+      {/if}
       <ul class="mt-2 ml-4">
-        {#each data.rooms as room}
+        {#each data.rooms as room, i}
           <li class="text-lg list-disc list-inside">
             <span class="font-medium">{room}:</span>
             <Link to={`room/${room}/teacher`}>[{$t("teacher-view")}]</Link>
             - <Link to={`room/${room}`}>[{$t("student-view")}]</Link> -
             <button
               class="text-red-600 hover:text-red-800"
-              on:click={() => deleteRoom(room)}
+              on:click={() => (showConfirmDelete = i)}
               >[{$t("home.delete-room")}]</button
             >
+            {#if showConfirmDelete === i}
+              <div class="inline ml-2 font-bold">
+                <p class="inline text-red-800">
+                  Are you sure you want to delete this room?
+                </p>
+                <button
+                  class="ml-1 text-red-600 hover:text-red-800 underline"
+                  on:click={() => deleteRoom(room)}>Yes</button
+                >
+                <button
+                  class="text-gray-600 hover:text-gray-800 underline"
+                  on:click={() => (showConfirmDelete = -1)}>Cancel</button
+                >
+              </div>
+            {/if}
           </li>
         {/each}
       </ul>
